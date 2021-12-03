9aae9652398c8726ac65ee94d50013dc5d8f7562
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-12-02 23:08:05 -0500

    Agregando addEnrrollments a los schemas y resolvers del modulo enrollments

--- src/modules/enrollments/enrollment.resolver.js
+++ src/modules/enrollments/enrollment.resolver.js
@@ -3,27 +3,36 @@ import {Enrollments} from './enrollment.module.js';
 import {Projects} from '../projects/project.module.js';
 import {Users} from '../users/user.module.js';
 
-const allEnrollments = async () => {
+const addEnrollment = async (parent, args, context, info) => {
+  let enrollment = new Enrollments(args.input);
+  enrollment = await enrollment.save();
+  return enrollment;
+}
+
+const allEnrollments = async (parent, args, context, info) => {
   const enrollments = await Enrollments.find();
   return enrollments;
 };
 
-const project = async (parent) => {
+const project = async (parent, args, context, info) => {
   const project = await Projects.findById(parent.project_id);
   return project;
 };
 
-const student = async (parent) => {
+const student = async (parent, args, context, info) => {
   const student = await Users.findById(parent.user_id);
   return student;
 };
 
 export default {
     Query: {
-        allEnrollments
+      allEnrollments
+    },
+    Mutation: {
+      addEnrollment
     },
     Enrollment: {
-        project,
-        student,
+      project,
+      student,
     }
 }
